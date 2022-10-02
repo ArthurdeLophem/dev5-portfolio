d4b9619ec081651bc9b978f134a64f6112e475d4
r0808 <arthurdelophem3@gmail.com>
2022-10-02 14:39:24 +0200

    feat: fetch forecast-data and save to localstorage

--- dev-lab-3/le-weather/public/js/weather.js
+++ dev-lab-3/le-weather/public/js/weather.js
@@ -1,6 +1,8 @@
 export default class Weather {
     constructor() {
-        this.weatherData = [];
+        this.apiKey = '5a52e4b0665830a2157245b3b43ef76a'
+        this.currentWeatherData = [];
+        this.ForecastData = [];
         this.getLocation();
         this.lat = 0;
         this.lng = 0;
@@ -8,28 +10,47 @@ export default class Weather {
 
     getLocation() {
         navigator.geolocation.getCurrentPosition(this.locationSucces.bind(this));
+
     }
 
     locationSucces(location) {
         this.lat = location.coords.latitude;
         this.lng = location.coords.longitude;
         //console.log(this.lat, this.lng);
-    }
-
-    static fetcher() {
-
+        this.searchData();
+        //localStorage.clear();
     };
 
-    static saveData() {
-
+    getCurrent() {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lng}&appid=${this.apiKey}`;
     };
 
-    static searchData() {
-        if (!localStorage.getItem('weatherData')) {
-            this.fetcher();
+    getForecast() {
+        let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.lat}&lon=${this.lng}&appid=${this.apiKey}`;
+        fetch(url).then(response => {
+            //console.log(response);
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            this.weatherData = data;
+            this.saveData()
+        }).catch(err => {
+            console.log(err);
+        })
+    };
+
+    saveData() {
+        localStorage.setItem('currentWeatherData', JSON.stringify(this.currentWeatherData));
+        localStorage.setItem('ForecastData', JSON.stringify(this.ForecastData));
+    };
+
+    searchData() {
+        if (!localStorage.getItem('weatherData') || localStorage.getItem('weatherData').length === 0) {
+            this.getCurrent();
+            this.getForecast();
         }
         else {
-            displayData(JSON.parse(localStorage.getItem('weatherData')));
+            //displayData(JSON.parse(localStorage.getItem('weatherData')));
         }
     };
 
