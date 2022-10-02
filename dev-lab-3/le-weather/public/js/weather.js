1cbe0323cba9a59fce6d8e7ae4a0f1dc4a0162fe
r0808 <arthurdelophem3@gmail.com>
2022-10-02 13:59:25 +0200

    feat: get user location data

--- dev-lab-3/le-weather/public/js/weather.js
+++ dev-lab-3/le-weather/public/js/weather.js
@@ -1,10 +1,19 @@
 export default class Weather {
     constructor() {
         this.weatherData = [];
+        this.getLocation();
+        this.lat = 0;
+        this.lng = 0;
     };
 
     getLocation() {
-        navigator.geolocation.getCurrentPosition(this.locationSucces.bind(this), this.locationError.bind(this));
+        navigator.geolocation.getCurrentPosition(this.locationSucces.bind(this));
+    }
+
+    locationSucces(location) {
+        this.lat = location.coords.latitude;
+        this.lng = location.coords.longitude;
+        //console.log(this.lat, this.lng);
     }
 
     static fetcher() {
