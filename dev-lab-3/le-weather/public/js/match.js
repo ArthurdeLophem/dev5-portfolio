2befae6c00e72c6312af2bd76ae1d27be5a2a11b
r0808 <arthurdelophem3@gmail.com>
2022-10-05 11:13:43 +0200

    feat: sort matches by today + save to array

--- dev-lab-3/le-weather/public/js/match.js
+++ dev-lab-3/le-weather/public/js/match.js
@@ -1,10 +1,23 @@
 export default class Match {
     constructor() {
-        this.apiKey = '';
         this.MatchData = [];
+        this.getToday();
         this.searchMatches();
+        this.date;
     };
+
+    getToday() {
+        var d = new Date();
+        let time = d.toLocaleString("en-CA", { timeZone: 'America/New_York' });
+        let day = time.slice(8, 10);
+        let month = time.slice(5, 7);
+        let year = time.slice(0, 4);
+        let date = year + month + day
+        this.date = date.toString();
+    }
+
     getMatches() {
+        console.log('here');
         fetch('http://data.nba.net/prod/v1/2022/schedule.json')
             .then(response => {
                 //console.log(response);
@@ -13,15 +26,25 @@ export default class Match {
                 console.log(data);
                 this.MatchData = data;
                 console.log(this.MatchData);
-
+                this.sortMatchesToday();
             }).catch(err => {
                 console.log(err);
             })
     };
 
     sortMatchesToday() {
+        console.log(this.MatchData.league.standard);
+        let array = [];
+        this.MatchData.league.standard.forEach(item => {
+            //console.log(item);
+            if (item.startDateEastern === this.date) {
+                array.push(item);
+            }
+        });
+        console.log(array);
+        this.MatchData = array;
         this.saveMatches();
-        this.displayMatch();
+        //this.displayMatch();
     }
 
     saveMatches() {
@@ -29,10 +52,11 @@ export default class Match {
     };
 
     searchMatches() {
+        //localStorage.removeItem('MatchData');
         if (!localStorage.getItem('MatchData')) {
             this.getMatches();
         } else {
-            this.currentWeatherData = JSON.parse(localStorage.getItem('MatchData'));
+            this.MatchData = JSON.parse(localStorage.getItem('MatchData'));
             this.sortMatchesToday();
         }
     };
